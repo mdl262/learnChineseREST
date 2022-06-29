d22bb05ac8b611c368092504ca8543ead5f44870
mdl262 <70067154+mdl262@users.noreply.github.com>
2022-06-29 13:19:18 -0400

    Add files via upload
    Updated CORS policy

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const express = require('express'),
     app = express(),
-    //bodyParser = require('body-parser'),
-    //fs = require('fs'),
     url = require('url'),
     port = process.env.PORT || 4000,
+    cors = require('cors')
     https = require('https');
 
 var translate = require('google-translate-api-x');
 
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }))
-
-/*const routes = require('./routes/routes.js')(app, fs);*/
-
-//let url = new URL(req.protocal + "://" + req.get('host') + req.originalUrl);
-//const url = new URL("https://url.com")
+app.use(
+    cors({
+        origin: "*",
+    })
+)
 
 app.get('/',async function (req, res) {
     this_url = url.parse(req.url, true);
@@ -23,16 +20,9 @@ app.get('/',async function (req, res) {
     query = this_url.query.query || ""
     console.log({"query":query})
 
-    result = fetchResult(query)
     result = await translate(query, { from: 'zh-CN', to: 'en' });
     res.send(result)
 });
-
-async function fetchResult(query) {
-    const response = await translate(query, { from: 'zh-CN', to: 'en' });
-    console.log(response);
-    return response;
-}
 
 app.listen(port, () => {
     console.log('Server started on: ' + port);
